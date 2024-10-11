3d60f170230589d61b0b978ca287246bb716b035
UrbanaC22 <urbanachowdhury224@gmail.com>
2024-10-11 22:14:23 +0600

    Add files via upload

---
+++ rosserial_2310077/scripts/sensor_node.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Float32MultiArray
+from geometry_msgs.msg import Twist
+
+class sensor_node:
+
+    def __init__(self) -> None:
+        rospy.init_node('sensor_info', anonymous=True)
+
+        self.pub=rospy.Publisher('/cmd_vel',Twist, queue_size=10)
+
+        rospy.Subscriber('/sensor_data', Float32MultiArray, self.callback)
+
+    def callback(self, sensor_data):
+    
+     velocity=Twist()
+
+     velocity.linear.x=sensor_data[0]
+
+    
+     while sensor_data[1]<=20:
+          velocity.linear.x=0
+          velocity.angular.z=sensor_data[0]
+
+     velocity.linear.x=sensor_data[0]
+
+
+          
+     self.pub.publish(velocity)
+
+if __name__=='__main__':
+   node= sensor_node()
+   rospy.spin
+
